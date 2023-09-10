d9f46f90848c8cbd71f3ec50cca22c059177867d
Sudarev_D <89365265993@mail.ru>
2023-09-10 18:19:34 +0700

    pre -final commit for HW_4

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from catalog.views import index, contact
+from catalog.views import index, contact, product
 
 # Настраиваем пути для главной страницы и страницы с обратной связью пользователя
 
 urlpatterns = [
     path('', index),
     path('contacts/', contact),
+    path('product/<pk>', product),
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,11 +1,24 @@
 from django.shortcuts import render
+from catalog.models import Category, Product
+
+
+
 
 def index(request):
-    """
-    Контроллер для отображения домашней страницы
-    :param request: параметры пользователя
-    """
-    return render(request, 'catalog/index.html')
+    products_list = Product.objects.all()
+    context = {
+        'objects_list': products_list
+    }
+    return render(request, 'catalog/index.html', context)
+
+
+
+# def index(request):
+#     """
+#     Контроллер для отображения домашней страницы
+#     :param request: параметры пользователя
+#     """
+#     return render(request, 'catalog/index.html')
 
 
 def contact(request):
@@ -19,3 +32,12 @@ def contact(request):
         message = request.POST.get('message')
         print(f'You have new message from {name}({email}): {message}')
     return render(request, 'catalog/contacts.html')
+
+
+def product(request, pk):
+    product = Product.objects.get(pk=pk)
+    context = {
+        'product': product
+    }
+
+    return render(request, 'catalog/product.html', context=context)
